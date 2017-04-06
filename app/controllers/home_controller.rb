b20bb13c247de03ddfcc122fa0281321f28dffe8
Choiminjune <mju6229@gmail.com>
2017-04-06 09:34:58 +0000

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+    
+    def index
+        
+    end
+    
+    def write
+        
+        @title = params[:title]
+        @content = params[:content]
+    end
+    
+end
